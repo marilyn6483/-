9bb8d499c43474e479d8bf7e7687731699ee1f0e
chenyy <1280129660@qq.com>
2018-11-23 23:51:13 +0800

    增加计算损失函数的异常捕获处理

--- playML/gradient_descent.py
+++ playML/gradient_descent.py
@@ -23,10 +23,14 @@ def J(X_b, y, theta):
     :param theta:
     :return: 损失函数
     """
-    return np.sum((y - X_b.dot(theta)) ** 2) / len(X_b)
+    try:
+        print(np.sum((y - X_b.dot(theta)) ** 2) / len(X_b))
+        return np.sum((y - X_b.dot(theta)) ** 2) / len(X_b)
+    except:
+        return float('inf')
 
 
-def gradient_descent(X_b, y, init_theta, eta, n_iters=1e4, eplison=1e-8):
+def gradient_descent(X_b, y, init_theta, eta, n_iters=10, eplison=1e-8):
     """
     :param init_theta: theta初始值
     :param eta: 系数eta
@@ -42,7 +46,8 @@ def gradient_descent(X_b, y, init_theta, eta, n_iters=1e4, eplison=1e-8):
         last_theta = theta
         theta = theta - eta * gradient
         # print("in")
-        if abs(J(x_b, y, theta) - J(X_b, y, last_theta)) < eplison:
+        print(theta)
+        if (abs(J(x_b, y, theta) - J(X_b, y, last_theta)) < eplison):
             break
         n = n + 1
         # print(theta)
@@ -58,7 +63,7 @@ if __name__ == "__main__":
     x_b = np.hstack([np.ones((len(x), 1)), x])
     init_theta = np.zeros(x_b.shape[1])
 
-    eta = 0.001
+    eta = 0.01
     # print(x_b)
     # print(J(x_b, y, init_theta))
     print(gradient_descent(x_b, y, init_theta, eta))
